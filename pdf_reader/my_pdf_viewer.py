fdea33e42e354e8946710fd9660f10a0208be627
Manuel I. Gómez G <16570057+Nachitox123@users.noreply.github.com>
2024-02-07 00:53:46 -0700

    Added scrollbars
    Scrollbars added to thumbnails and main sections.

--- pdf_reader/my_pdf_viewer.py
+++ pdf_reader/my_pdf_viewer.py
@@ -27,13 +27,16 @@ def open_pdf():
             entry_integer.delete(0, tk.END)
             entry_integer.insert(0, "1")
             pages_label.config(text=f"/ {total_pages}")
-            window.title(f"PDF Viewer - {os.path.basename(file)}")
+            window.title(f"PDF Viewer - LOADING")
 
             # Load thumbnails for all pages
             load_thumbnails()
 
             # Load all pages
             load_pages()
+
+            # Set window title
+            window.title(f"PDF Viewer - {os.path.basename(file)}")
 
         except Exception as e:  # If it fails, show an error message
             messagebox.showerror("Error", f"Failed to open the PDF file: {str(e)}")
@@ -64,20 +67,20 @@ def load_pages():
                 widest_page = image.width
 
         # Update scroll regions
-        display_panel.config(scrollregion=(0, 0, widest_page, pos_y))
+        display_panel.config(scrollregion=(0, 0, widest_page + 30, pos_y))
 
 # Function to load thumbnails
 def load_thumbnails():
     thumbnail_panel.delete("all")  # Clear the canvas
 
-    pos_y = pos_x = 6
+    pos_y = pos_x = 10
     widest_thumbnail = 0
 
     for page_num in range(0, total_pages):
         page = pdf_file[page_num]
 
         # Thumbnail preview in thumbnails_panel
-        scale_factor = 0.163
+        scale_factor = 0.173
         thumbnail = page.get_pixmap(matrix=fitz.Matrix(scale_factor, scale_factor))
         tk_thumbnail = tk.PhotoImage(data=thumbnail.tobytes(), width=thumbnail.width, height=thumbnail.height)
         thumbnail_panel.create_image(pos_x, pos_y, anchor=tk.NW, image=tk_thumbnail)
@@ -110,21 +113,28 @@ def exit_app():
     if result:
         window.destroy()
 
-# Function to move to next page
-def next_page():
-    current_page = int(entry_integer.get())
-    if 1 <= current_page < total_pages:
-        entry_integer.delete(0, tk.END)
-        entry_integer.insert(0, str(current_page + 1))
-        load_pages(current_page + 1)
+# Function to move to specific page
+def jump_to_page(page_num):
+    page_num -= 1
+    print(f"New page: {page_num}")
 
 # Function to move to previous page
 def previous_page():
     current_page = int(entry_integer.get())
     if 2 <= current_page <= total_pages:
+        new_page = current_page - 1
         entry_integer.delete(0, tk.END)
-        entry_integer.insert(0, str(current_page - 1))
-        load_pages(current_page - 1)
+        entry_integer.insert(0, str(new_page))
+        jump_to_page(new_page)
+
+# Function to move to next page
+def next_page():
+    current_page = int(entry_integer.get())
+    if 1 <= current_page < total_pages:
+        new_page = current_page + 1
+        entry_integer.delete(0, tk.END)
+        entry_integer.insert(0, str(new_page))
+        jump_to_page(new_page)
 
 # Create the main window
 window = tk.Tk()
@@ -148,12 +158,12 @@ window.geometry(f"{defaultWidth }x{defaultHeight }+{x_coordinate}+{y_coordinate}
 
 # # Set app icon dynamically
 script_directory = os.path.dirname(os.path.realpath(__file__))
-icon_path = os.path.join(script_directory, "pdf512.ico" if os.name == "nt" else "pdf512.icns")
+icon_path = os.path.join(script_directory, "assets", "pdf512.ico" if os.name == "nt" else "pdf512.icns")
 window.iconbitmap(default=icon_path)
 
 # Make rows and columns resizable
 window.rowconfigure(1, weight=1)
-window.columnconfigure(1, weight=1)
+window.columnconfigure(2, weight=1)
 
 # Top Menu Bar
 menu_bar = tk.Menu(window)
@@ -183,6 +193,11 @@ window.config(menu=menu_bar)
 thumbnail_panel = tk.Canvas(window, width=0, bg="gray", scrollregion=(0, 0, 0, 0))
 thumbnail_panel.grid(row=0, column=0, rowspan=2, sticky="nsew")
 
+thumbnail_scrollbar = tk.Scrollbar(window, width=15, orient="vertical", command=thumbnail_panel.yview)
+thumbnail_scrollbar.grid(row=0, column=1, rowspan=2, sticky="nse")
+
+thumbnail_panel.config(yscrollcommand=thumbnail_scrollbar.set)
+
 # # Scroll for thumbnail_panel
 thumbnail_panel.bind("<MouseWheel>", lambda event: on_mousewheel(event, thumbnail_panel))
 # thumbnail_panel.bind("<Control-MouseWheel>", lambda event: on_control_mousewheel(event, thumbnail_panel))
@@ -192,24 +207,33 @@ nav_frame = tk.Frame(window, width=300)
 nav_frame.grid(row=2, column=0, pady=(5, 5))
 
 # Integer input entry
-entry_integer = tk.Entry(nav_frame, width=5)
-entry_integer.grid(row=0, column=0, padx=(0, 30))
+entry_integer = tk.Entry(nav_frame, width=6)
+entry_integer.grid(row=0, column=0, padx=(20, 30))
 entry_integer.insert(0, 0)
 
 # Label for pages
 pages_label = tk.Label(nav_frame, text=f"/ {total_pages}")
-pages_label.grid(row=0, column=0, columnspan=2, padx=(30, 0))
+pages_label.grid(row=0, column=0, columnspan=2, padx=(40, 0))
 
 # Buttons with arrows
 button_up = tk.Button(nav_frame, text="Previous", command=previous_page)
-button_up.grid(row=1, column=0, padx=(10, 50))
+button_up.grid(row=1, column=0, padx=(20, 50))
 
 button_down = tk.Button(nav_frame, text="Next", command=next_page)
-button_down.grid(row=1, column=0, columnspan=2, padx=(60, 0))
+button_down.grid(row=1, column=0, columnspan=2, padx=(70, 0))
 
 # Canvas for display
 display_panel = tk.Canvas(window, width=0, bg="gray", scrollregion=(0, 0, 0, 0))
-display_panel.grid(row=0, column=1, rowspan=3, columnspan=2, sticky="nsew") # Might change to rowspan=2
+display_panel.grid(row=0, column=2, rowspan=3, sticky="nsew") # Might change to rowspan=2
+
+# Scrollbar for display
+display_scrollbar_x = tk.Scrollbar(window, orient="horizontal", command=display_panel.xview)
+display_scrollbar_x.grid(row=2, column=2, sticky="wes")
+
+display_scrollbar_y = tk.Scrollbar(window, orient="vertical", command=display_panel.yview)
+display_scrollbar_y.grid(row=0, column=3, rowspan=3, sticky="nse")
+
+display_panel.config(xscrollcommand=display_scrollbar_x.set, yscrollcommand=display_scrollbar_y.set)
 
 # Scroll for display_panel
 display_panel.bind("<MouseWheel>", lambda event: on_mousewheel(event, display_panel))
